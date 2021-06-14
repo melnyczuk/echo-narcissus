d86c0dd702358aec63876f4103ea18332140688d
how melnyczuk <how@melnycz.uk>
2021-06-14 18:52:23 +0100

    Update renderer

--- src/client/renderer.ts
+++ src/client/renderer.ts
@@ -1,11 +1,13 @@
-import console from 'console';
 import {
+  console,
+  document,
+  HTMLCanvasElement,
   HTMLVideoElement,
   MediaStream,
   window,
 } from 'globalthis/implementation';
 import moveNet from '../services/moveNet';
-import { poseMsg, Settings, settingsMsg } from './messages';
+import { MsgType, poseMsg, Settings, settingsMsg } from './messages';
 
 const BASE_URL = '127.0.0.1:4242';
 
@@ -19,24 +21,45 @@ const settings: Settings = {
 
 const streamContraints: MediaStreamConstraints = {
   video: {
-    width: { max: 3840, ideal: 3840, min: 1280 },
-    height: { max: 2160, ideal: 2160, min: 720 },
+    width: { max: 3840, ideal: 1280, min: 1280 },
+    height: { max: 2160, ideal: 720, min: 720 },
     frameRate: { max: 30, ideal: 30, min: 25 },
   },
 };
 
-const animationLoop = (videoElm: HTMLVideoElement) => () => {
-  window.requestAnimationFrame(async () => {
-    const [pose] = await moveNet.run(videoElm);
-    socket.send(poseMsg(pose));
-    animationLoop(videoElm);
-  });
+const canvas = document.getElementById('display') as HTMLCanvasElement;
+const fpsElement = document.getElementById('fps');
+
+const drawLivePose = moveNet.draw('red', {points:true, frame:true});
+const drawGeneratedPose = moveNet.draw('green');
+
+const renderFPS =
+  (then: number): FrameRequestCallback =>
+  (now: number) => {
+    const elapsedTime = now - then;
+    const fps = (1 / elapsedTime) * 1000;
+    fpsElement.innerText = `${Math.floor(fps)} fps`;
+    window.requestAnimationFrame(renderFPS(now));
+  };
+
+const bindVideoToMoveNet = async (videoElm: HTMLVideoElement) => {
+  const moveNetDetector = await moveNet.createDetector();
+
+  const loop = async () => {
+    const [pose] = await moveNetDetector.estimatePoses(videoElm);
+    drawLivePose(canvas, pose);
+    const normalisedPose = moveNet.normalise(canvas, pose);
+    socket.send(poseMsg(normalisedPose));
+    window.requestAnimationFrame(loop);
+  };
+
+  return loop;
 };
 
-const handleStream = (stream: MediaStream): void => {
+const handleStream = async (stream: MediaStream): Promise<void> => {
   const videoElm = document.getElementById('webcam') as HTMLVideoElement;
   videoElm.srcObject = stream;
-  videoElm.onloadeddata = animationLoop(videoElm);
+  videoElm.onloadeddata = await bindVideoToMoveNet(videoElm);
 };
 
 socket.onopen = (event): void => {
@@ -46,6 +69,7 @@ socket.onopen = (event): void => {
     .getUserMedia(streamContraints)
     .then(handleStream)
     .catch(console.error);
+  window.requestAnimationFrame(renderFPS(Date.now() / 1000));
 };
 
 socket.onclose = (event): void => {
@@ -57,4 +81,8 @@ socket.onerror = (): void => console.error('Socket error <=');
 socket.onmessage = async (event) => {
   const { data, type } = JSON.parse(event.data);
   console.log(`${type} <=`, data);
+
+  if (type === MsgType.POSE) {
+    drawGeneratedPose(canvas, data);
+  }
 };
